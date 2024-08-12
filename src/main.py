39bc335467678e8f40d68259fab618d72696cb6d
Juancito2085 <jcbrunello@hotmail.com>
2024-08-12 18:23:09 -0300

    Segundo commit

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import psse34
+import psspy
+import LecturaDatos as LD
+import VerificacionDatos as VD
+import CalculoReserva as CR
+import PruebaExcel as PD
+ 
+# sys_path_PSSE=r'E:\PSS\PSSPY34'
+# sys.path.append(sys_path_PSSE)
+# sys_path_PSSE=r'E:\PSS\PSSPY34'
+# sys.path.append(r'C:/Users/juan/AppData/Local/Programs/Python/Python312/Lib/site-packages')
+ 
+# os_path_PSSE=r'E:\PSS\PSSBIN'
+# os.environ['PATH'] += ';' + os_path_PSSE
+# os.environ['PATH'] += ';' + sys_path_PSSE
+ 
+#importacion de librerias necesarias
+ 
+import redirect
+import datetime
+import re
+import tkinter
